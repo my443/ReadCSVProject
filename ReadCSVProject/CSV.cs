9223eb040b80b738f74f334c15c98d96eb68e11b
John van Dijk <fortyfour-three@teksavvy.com>
2024-03-20 06:08:44 -0400

    Refactored the reading CSV into a separate class.

---
+++ ReadCSVProject/CSV.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace ReadCSVProject
+{
+    public class CSVAction
+    {
+
+        /// <summary>
+        /// Reads a csv, returns a table.
+        /// </summary>
+        /// <returns></returns>
+        public DataTable readCSV(string pathToFile)
+        {
+
+            DataTable dt = new DataTable();
+
+            // CSV DataReader
+            // https://joshclose.github.io/CsvHelper/examples/csvdatareader/
+            using (var reader = new StreamReader(pathToFile))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                using (var dr = new CsvDataReader(csv))
+                {
+
+                    dt.Load(dr);
+                }
+            }
+
+            return dt;
+        }
+
+        public void printDataTable(DataTable dt)
+        {
+            // Print contents of DataTable.
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                string row = "";
+                foreach (var item in dataRow.ItemArray)
+                {
+                    row = row + item + " - ";
+
+                }
+                Console.WriteLine(row);
+            }
+        }
+
+    }
+}
--- ReadCSVProject/Program.cs
+++ ReadCSVProject/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CsvHelper;
-using System;
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using ReadCSVProject;
 using System.Data;
-using System.Globalization;
 
 class GFG
 {
@@ -12,36 +12,11 @@ class GFG
     {
         Console.WriteLine("Start Reading a csv to data table.");
 
+        string pathToFile = """..\..\..\Book1.csv""";           // has to be like this so that it looks at the right levels.
+        CSVAction csv = new CSVAction();
+        DataTable csvData =  csv.readCSV(pathToFile);
+        csv.printDataTable(csvData);
 
-        DataTable dt = new DataTable();
-
-        
-        string pathToFile = """..\..\..\Book1.csv""";           // has to be like this so that it looks at the right levels. 
-
-        // CSV DataReader
-        // https://joshclose.github.io/CsvHelper/examples/csvdatareader/
-        using (var reader = new StreamReader(pathToFile))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            var records = csv.GetRecords<dynamic>();
-            using (var dr = new CsvDataReader(csv))
-            {
-                
-                dt.Load(dr);
-            }
-        }
-
-        // Print contents of DataTable.
-        foreach (DataRow dataRow in dt.Rows)
-        {
-            string row = "";
-            foreach (var item in dataRow.ItemArray)
-            {
-                row = row + item + " - ";
-                
-            }
-            Console.WriteLine(row);
-        }
 
         Console.WriteLine("CSV Read to Data Table!");
     }
